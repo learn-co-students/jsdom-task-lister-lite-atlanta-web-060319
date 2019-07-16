7894a0de50e2e74d38cfe192aa66a78de6cc103e
Dylan Harris <dylanharris23@gmail.com>
2019-07-16 16:24:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let createForm = document.querySelector('#create-task-form')
+  createForm.addEventListener('submit', handleCreateForm)
 });
+
+function handleCreateForm(e) {
+  e.preventDefault()
+  let task = {user: e.target["new-task-description"].value}
+  createList(task)
+}
+
+function createList(task) {
+  let list = document.querySelector("#tasks")
+  let li = document.createElement('li')
+  li.innerHTML = task.user
+  list.appendChild(li)
+}
